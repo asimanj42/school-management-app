adbec2d14aef79326f5b1cbdcddd952a31b9e317
Asiman42 <asimanmemmedli123321@gmail.com>
2023-05-22 19:55:29 +0400

    changed

---
+++ src/main/java/az/company/schoolmanageapp/exception/ScheduleNotFoundException.java
@@ -0,0 +1,7 @@
+package az.company.schoolmanageapp.exception;
+
+public class ScheduleNotFoundException extends RuntimeException {
+    public ScheduleNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/az/company/schoolmanageapp/exception/TeacherNotFoundException.java
@@ -0,0 +1,7 @@
+package az.company.schoolmanageapp.exception;
+
+public class TeacherNotFoundException extends RuntimeException {
+    public TeacherNotFoundException(String message) {
+        super(message);
+    }
+}
